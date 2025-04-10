e3c2feeb11513d8e91e6569ce0258651764423d8
Ajay Kumar Kondaboina <168417774+AjayKumar239@users.noreply.github.com>
2025-04-10 10:58:41 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class hello{
     public static void main(String[] args) {
-        System.out.println("WellCome CICD");
+        System.out.println("WellCome CICD_Devops");
         System.out.println("WellCome Form");
 
     }
